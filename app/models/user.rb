dde7ee7296c069145eda3e458ac3e1d9bb51b8c5
Tara Koomen <tarakoomen88@gmail.com>
2024-04-25 22:20:58 +1000

    Added more changes to allow cloudinary image upload

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :messages
   has_many :groups, through: :group_users
+  has_one_attached :photo
 
   validates :first_name, presence: true
   validates :last_name, presence: true
