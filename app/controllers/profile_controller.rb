972d483daf10d673431be754792d1a7caa1197bb
Tara Koomen <tarakoomen88@gmail.com>
2024-04-27 14:17:38 +1000

    Added a profile controller and a profile edit view page

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,7 +1,24 @@
 class ProfileController < ApplicationController
+  before_action :set_user, only: %i[edit update]
+
   def edit
   end
 
   def update
+    if @user.update(user_params)
+      redirect_to @user, notice: "Your profile was successfully updated.", status: :see_other
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:bio, :due_date, :post_code)
   end
 end
