41c764907724f26666eb84f6e7d1a5e937d43367
Tara Koomen <tarakoomen88@gmail.com>
2024-04-27 22:16:20 +1000

    Updated the application controller and user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,12 @@ class User < ApplicationRecord
   has_one_attached :photo
 
   validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :nickname, presence: true, uniqueness: true
+  before_validation :strip_email
+  validates :email, presence: true, format: {
+    with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+    }
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
