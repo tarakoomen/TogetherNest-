b46b53e61ee79f9294560a150af4daace30cc078
Tara Koomen <tarakoomen88@gmail.com>
2024-04-27 16:20:12 +1000

    WIP

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   has_many :groups, through: :group_users
   has_one_attached :photo
 
+  validates :first_name, presence: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
