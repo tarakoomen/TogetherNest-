9b0210de4756d2deadfc32fd06b38b554f82eda5
Tara Koomen <tarakoomen88@gmail.com>
2024-04-30 20:02:12 +1000

    Updated groups controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,18 @@
 class GroupsController < ApplicationController
+  def index
+    @groups = Group.all
+    # sort by / order by post_code (descending)
+    @group.user = current_user
+    @user_post_code = current_user.post_code
+    # @group_post_code = @group.post_code
+  end
+
+  def new
+  end
+
+  def create
+  end
+
   def show
     @group = Group.find(params[:id])
     @group_type = @group.group_type
