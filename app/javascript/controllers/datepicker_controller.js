d1a385476f238a46fc35c470270e70d1897c0449
Matthew Fox <mattfoxsydney@gmail.com>
2024-05-04 09:26:37 +1000

    dashboard update

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -3,6 +3,17 @@ import flatpickr from "flatpickr"; // You need to import this to use new flatpic
 
 export default class extends Controller {
   connect() {
-    flatpickr(this.element)
+    flatpickr(this.element, {
+      dateFormat: "d/m/Y", // Set the date format to dd/mm/yyyy
+      onChange: function(selectedDates, dateStr, instance) {
+        // Split the date string by '/' and reverse the array to change the order
+        var parts = dateStr.split('/');
+
+        // Join the parts with '/'
+        var formattedDate = parts.join('/');
+        // Set the formatted date value back to the input
+        instance.input.value = formattedDate;
+      }
+    });
   }
 }
