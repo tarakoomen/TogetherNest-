3b19ad8e5df095203e941d364018362935b94511
Tara Koomen <tarakoomen88@gmail.com>
2024-05-04 14:19:02 +1000

    Added a find a mentor link to the mentorship show page

--- config/routes.rb
+++ config/routes.rb
@@ -23,8 +23,6 @@ Rails.application.routes.draw do
   resource :profile, only: [:edit, :update]
   resource :mentor_profile, only: [:edit, :update]
 
-  resources :cars
-
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
