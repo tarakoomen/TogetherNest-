1128fcdf22ed2f5b3365fc5fd3a15d8988d9c033
Tara Koomen <tarakoomen88@gmail.com>
2024-05-10 22:08:52 +1000

    Updated text on the mentorship info page and wrote the first version code for minimum 2 yrs parent experience

--- app/controllers/mentor_profiles_controller.rb
+++ app/controllers/mentor_profiles_controller.rb
@@ -1,10 +1,11 @@
 class MentorProfilesController < ApplicationController
   def edit
+    # current_user.parent_experience >= 2
   end
 
   def update
     if current_user.update(mentor_params)
-      redirect_to group_path(current_user.groups.where(group_type: "parent community").first), notice: "Your answers were succesfully saved", status: :see_other
+      redirect_to group_path(current_user.groups.where(group_type: "parent community").first), notice: "Your answers have been succesfully saved", status: :see_other
     else
       render :edit, status: :unprocessable_entity
     end
